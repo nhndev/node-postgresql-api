ac551b08c5d69041c243ecf304e2db59b1952886
nhndev <ngohuunhan2001pro@gmail.com>
2023-05-11 22:28:18 +0700

    vercel

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ const bookRouter = require('./routes/book.router')
 
 app.use("/api/v1/books", bookRouter)
 
-app.listen(5000, () => console.log("Server is running on port 5000"))
+app.listen(process.env.PORT, () => console.log("Server is running on port 5000"))
