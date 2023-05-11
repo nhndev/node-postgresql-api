e82bb75f98093b6add06641029d08b79beb1d9f7
nhndev <ngohuunhan2001pro@gmail.com>
2023-05-11 22:24:17 +0700

    vercel

--- database.js
+++ database.js
@@ -1,11 +1,7 @@
 const { Pool } = require('pg')
  
 const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DATABASE,
-  password: process.env.POSTGRES_PASSWORD,
-  port: 5431,
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 })
 
 pool.connect((err) => {
